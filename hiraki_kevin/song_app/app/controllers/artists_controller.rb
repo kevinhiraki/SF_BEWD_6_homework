75a249a04742ac2cd0079b531aa650aa55d04b4f
Kevin Hiraki <kevinhiraki@Kevin-Hirakis-MacBook-Pro.local>
2014-10-19 13:51:27 -0700

    Added RESTful actions and got them to work

--- hiraki_kevin/song_app/app/controllers/artists_controller.rb
+++ hiraki_kevin/song_app/app/controllers/artists_controller.rb
@@ -1,6 +1,7 @@
 class ArtistsController < ApplicationController
 
 def index
+	@artists = Artist.all
 end
 
 def new
@@ -23,12 +24,13 @@ end
 
 private
 
+#DRY: used by show, edit, destroy
 def artist_params
 	params.require(:artist).permit(:name, :description, :image)
 end
 
-def find.artist
-	Artist.find(params[:id])
-end
+# def find.artist
+# 	Artist.find(params[:id])
+# end
 
 end
